9d4f3239264a868f19187cdfe93207fc15156a56
Weiping <hihiweiping@gmail.com>
2021-10-26 17:50:56 +0800

    my-site

---
+++ src/pages/about.js
@@ -0,0 +1,41 @@
+import React from "react"
+import Layout from "../components/Layout"
+
+const About = () => {
+  return (
+    <Layout>
+      <div>
+        <hi>About Page</hi>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Earum nisi
+          magni quis expedita sint ullam. Quis provident, quas est inventore,
+          omnis fugit amet aut sequi accusantium perspiciatis veritatis?
+          Exercitationem voluptatibus odit iusto assumenda magnam cupiditate
+          culpa nihil recusandae, pariatur doloremque maiores, dolorum facere
+          voluptatum laboriosam praesentium molestiae consectetur mollitia?
+          Placeat?
+        </p>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Earum nisi
+          magni quis expedita sint ullam. Quis provident, quas est inventore,
+          omnis fugit amet aut sequi accusantium perspiciatis veritatis?
+          Exercitationem voluptatibus odit iusto assumenda magnam cupiditate
+          culpa nihil recusandae, pariatur doloremque maiores, dolorum facere
+          voluptatum laboriosam praesentium molestiae consectetur mollitia?
+          Placeat?
+        </p>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Earum nisi
+          magni quis expedita sint ullam. Quis provident, quas est inventore,
+          omnis fugit amet aut sequi accusantium perspiciatis veritatis?
+          Exercitationem voluptatibus odit iusto assumenda magnam cupiditate
+          culpa nihil recusandae, pariatur doloremque maiores, dolorum facere
+          voluptatum laboriosam praesentium molestiae consectetur mollitia?
+          Placeat?
+        </p>
+      </div>
+    </Layout>
+  )
+}
+
+export default About
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,36 @@
 import React from "react"
+import { graphql, Link } from "gatsby"
+import Img from "gatsby-image"
 
-export default function Home() {
-  return <div>Hello world!</div>
+import Layout from "../components/Layout"
+import * as styles from "../styles/home.module.css"
+
+export default function Home({ data }) {
+  return (
+    <Layout>
+      <section className={styles.header}>
+        <div>
+          <h2>Design</h2>
+          <h3>Develop & Deploy</h3>
+          <p>UX designer & web developer based in Manchester.</p>
+          <Link className={styles.btn} to="/projects">
+            My Portfolio Projects
+          </Link>
+        </div>
+        <Img fluid={data.file.childImageSharp.fluid} />
+      </section>
+    </Layout>
+  )
 }
+
+export const query = graphql`
+  query MyBanner {
+    file(relativePath: { eq: "banner.png" }) {
+      childImageSharp {
+        fluid {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+  }
+`
